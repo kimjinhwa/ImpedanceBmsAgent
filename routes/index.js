e370eba23e85d0b408e8aa75a6ea74aa5e9d982c
JinHwa. KIM <mvcjhkim@gmail.com>
2022-06-22 09:35:15 +0900

    백엔드 펑션을 만들고 데이타베이스에서 데이타를 읽고 system info를 출력하였다

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/impedancedb');
+// console.log("mongodb connection...");
+// var db = mongoose.connection;
+// db.on('error', function () {
+//   console.log('mongodb connetion Failed!');
+// });
+var db = require('../backend/database')
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'BMS관리 시스템' });
 });
 router.get('/login', function (req, res, next) {
   res.render('login/login', { title: 'Login Process' });
@@ -16,10 +26,18 @@ router.get('/systemConfigure', function (req, res, next) {
 
 router.post('/systemConfigure', (req, res) => {
 
-  const jsonFile = fs.readFileSync('databases/systemConfigureData.json', 'utf8');
-  console.log(req.body);
-  //console.log(jsonFile);
-  res.send(jsonFile);
+  var cursor = db.collection('bms_system').find();
+  var retString = [];
+  cursor.forEach((doc) => {
+    retString.push(doc);
+  }).then(() => {
+    res.send(retString);
+  });
 })
 
 module.exports = router;
+
+  //  console.log(retString);
+  // const jsonFile = fs.readFileSync('databases/systemConfigureData.json', 'utf8');
+  // console.log(req.body);
+  // res.send(jsonFile);
